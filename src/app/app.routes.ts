e8894ec0d9c7f960f21a1fadae37ce10ef83dd37
Arthur-Santos13 <ahsantos1301@gmail.com>
2026-05-01 11:34:08 -0300

    chore(frontend): initialize Angular project with standalone components

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            {
+                path: 'home',
+                loadComponent: () =>
+                    import('./pages/home/home').then((m) => m.HomeComponent),
+            },
+            {
+                path: 'items',
+                loadComponent: () =>
+                    import('./pages/items/items').then((m) => m.ItemsComponent),
+            },
+            {
+                path: 'recommendations',
+                loadComponent: () =>
+                    import('./pages/recommendations/recommendations').then(
+                        (m) => m.RecommendationsComponent
+                    ),
+            },
+        ],
+    },
+    { path: '**', redirectTo: 'home' },
+];
